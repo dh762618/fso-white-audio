25f64ad15cffa8e0a55486a2096e947c39ff279e
Devin Holt <dh762618@ohio.edu>
2023-02-16 01:43:59 -0500

    Added current test files

--- ReceivingTeensy/src/main.cpp
+++ ReceivingTeensy/src/main.cpp
@@ -1,9 +1,57 @@
-#include <Arduino.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AsyncAudioInputSPDIF3    spdifIn(false, false, 100, 20, 80);   //xy=307,222
+AudioAmplifier           amp2;           //xy=473,247
+AudioAmplifier           amp1;           //xy=477,211
+AudioOutputPT8211        i2s1;           //xy=740,223
+AudioConnection          patchCord1(spdifIn, 0, amp1, 0);
+AudioConnection          patchCord2(spdifIn, 1, amp2, 0);
+AudioConnection          patchCord3(amp2, 0, i2s1, 1);
+AudioConnection          patchCord4(amp1, 0, i2s1, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=106,226
+// GUItool: end automatically generated code
+
 
 void setup() {
-  // put your setup code here, to run once:
+  AudioMemory(12);
+  amp1.gain(1);
+  amp2.gain(1);
+  Serial.begin(57600);
+  while (!Serial);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  double bufferedTime=spdifIn.getBufferedTime();
+  Serial.print("buffered time [micro seconds]: ");
+  Serial.println(bufferedTime*1e6,2);
+  
+  
+  Serial.print("locked: ");
+  Serial.println(spdifIn.isLocked());
+  Serial.print("input frequency: ");
+  double inputFrequency=spdifIn.getInputFrequency();
+  Serial.println(inputFrequency);
+  Serial.print("anti-aliasing attenuation: ");
+  Serial.println(spdifIn.getAttenuation());
+  
+  Serial.print("resampling goup delay [milli seconds]: ");
+  Serial.println(spdifIn.getHalfFilterLength()/inputFrequency*1e3,2);
+  
+  Serial.print("half filter length: ");
+  Serial.println(spdifIn.getHalfFilterLength()); 
+  
+  double pUsageIn=spdifIn.processorUsage(); 
+  Serial.print("processor usage [%]: ");
+  Serial.println(pUsageIn);
+
+  Serial.print("max number of used blocks: ");
+  Serial.println(AudioMemoryUsageMax()); 
+
+  delay(500);
 }
--- TransmissionTeensy/src/main.cpp
+++ TransmissionTeensy/src/main.cpp
@@ -1,9 +1,55 @@
-#include <Arduino.h>
+/*
+ * A simple hardware test which receives audio from the audio shield
+ * Line-In pins and send it to the Line-Out pins and headphone jack.
+ *
+ * This example code is in the public domain.
+ */
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code4
+AudioFilterBiquad        biquad1;
+AudioInputI2S            i2s1;           //xy=200,69
+AudioOutputSPDIF3        spout1;
+AudioOutputI2S           i2s2;           //xy=365,94
+AudioAmplifier           amp1;
+AudioConnection          patchCord1(i2s1, 0, i2s2, 0);
+AudioConnection          patchCord2(i2s1, 1, i2s2, 1);
+AudioConnection          patchCord3(i2s1, 0, amp1, 0);
+AudioConnection          patchCord4(amp1, 0, biquad1, 0);
+AudioConnection          patchCord5(biquad1, 0, spout1, 0);
+AudioConnection          patchCord6(biquad1, 0, spout1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=302,184
+// GUItool: end automatically generated code
+
+
+const int myInput = AUDIO_INPUT_LINEIN;
+//const int myInput = AUDIO_INPUT_MIC;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  // Audio connections require memory to work.  For more
+  // detailed information, see the MemoryAndCpuUsage example
+  AudioMemory(12);
+  // Enable the audio shield, select input, and enable output
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(myInput);
+  sgtl5000_1.volume(0.7);
+  amp1.gain(2);
+  biquad1.setLowpass(0, 800, 0.707);
 }
 
+elapsedMillis volmsec=0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // every 50 ms, adjust the volume
+  if (volmsec > 50) {
+    float vol = analogRead(15);
+    vol = vol / 1023.0;
+    //audioShield.volume(vol); // <-- uncomment if you have the optional
+    volmsec = 0;               //     volume pot on your audio shield
+  }
 }
