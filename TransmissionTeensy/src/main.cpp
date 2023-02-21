72a3741ac909b2b144cbcc9d33ac838fa53db03d
Devin Holt <dh762618@ohio.edu>
2023-02-21 15:45:32 -0500

    Fixed lowpass and amp

--- TransmissionTeensy/src/main.cpp
+++ TransmissionTeensy/src/main.cpp
@@ -16,12 +16,14 @@ AudioInputI2S            i2s1;           //xy=200,69
 AudioOutputSPDIF3        spout1;
 AudioOutputI2S           i2s2;           //xy=365,94
 AudioAmplifier           amp1;
-AudioConnection          patchCord1(i2s1, 0, i2s2, 0);
-AudioConnection          patchCord2(i2s1, 1, i2s2, 1);
-AudioConnection          patchCord3(i2s1, 0, amp1, 0);
-AudioConnection          patchCord4(amp1, 0, biquad1, 0);
-AudioConnection          patchCord5(biquad1, 0, spout1, 0);
-AudioConnection          patchCord6(biquad1, 0, spout1, 1);
+AudioConnection          patchCord1(i2s1, 0, amp1, 0);
+AudioConnection          patchCord2(i2s1, 1, amp1, 1);
+AudioConnection          patchCord3(amp1, 0, biquad1, 0);
+AudioConnection          patchCord4(amp1, 1, biquad1, 1);
+AudioConnection          patchCord5(biquad1, 0, i2s2, 0);
+AudioConnection          patchCord6(biquad1, 1, i2s2, 1);
+AudioConnection          patchCord7(biquad1, 0, spout1, 0);
+AudioConnection          patchCord8(biquad1, 1, spout1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=302,184
 // GUItool: end automatically generated code
 
@@ -39,7 +41,7 @@ void setup() {
   sgtl5000_1.inputSelect(myInput);
   sgtl5000_1.volume(0.7);
   amp1.gain(2);
-  biquad1.setLowpass(0, 800, 0.707);
+  biquad1.setLowpass(0, 8900, 0.707);
 }
 
 elapsedMillis volmsec=0;
